cebf66279a0471130eb266d5f2c5a26ca1315667
Kevin Mendoza <kevin@xymilar.com>
2023-03-08 20:20:29 -0300

    colors theme

--- client-react/src/components/FloatingButton/FloatingButton.tsx
+++ client-react/src/components/FloatingButton/FloatingButton.tsx
@@ -5,12 +5,12 @@ export const FloatingButton = ({ onClick, loading, icon }: any) => {
 
     const styles: CSS.Properties  = {
         position: 'fixed', 
-        backgroundColor: '#4d315e',
+        backgroundColor: 'var(--primary)',
         border: 'none', 
         height: '40px', 
         width: '40px', 
         borderRadius: '40px',
-        color: '#fff',
+        color: 'var(--font)',
         fontSize: '24px',
         right: '40px',
         bottom: '30px',
