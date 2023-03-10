346d1e7b0914a3c1ccb26a664347b7582507ac79
Kevin Mendoza <kevin@xymilar.com>
2023-03-10 17:41:01 -0300

    login logout working

--- client-react-native/App.js
+++ client-react-native/App.js
@@ -1,8 +1,16 @@
+import { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Navigation } from './routes/Navigation';
 import { StatusBar } from 'react-native';
+import { useAuthStore } from './store/useAuthStore';
 
 export default function App() {
+
+  const getAuth = useAuthStore((state) => state.getAuth);
+
+  useEffect(() => {
+    getAuth();
+  }, []);
 
   return (
     <NavigationContainer>
