d09dd060b7fb2ef59614e8b62762f214abed50c6
Kevin Mendoza <kevin@xymilar.com>
2023-03-24 11:10:07 -0300

    authorization header

--- server-node/src/middlewares/auth.ts
+++ server-node/src/middlewares/auth.ts
@@ -3,20 +3,13 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
 export const auth = (req: Request, res: Response, next: NextFunction) => {
-    if (!req.cookies.token && !req.headers.authorization) res.status(403).send({ message: 'access denied' });
+    if (!req.headers.authorization) res.status(403).send({ message: 'Authorization token needed.' });
 
     try {
-        if (req.cookies.token) {
-            const decoded = jwt.verify(req.cookies.token, process.env.JWT_SECRET);
-            //console.log(11, decoded);
-            next();
-        }
-        if (req.headers.authorization) {
-            const decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
-            //console.log(16, decoded);
-            next();
-        }
+        const decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+        //console.log(16, decoded);
+        next();
     } catch (error: any) {
-        res.status(401).send({ message: 'access denied' });
+        res.status(401).send({ message: 'Invalid authorization token.' });
     }
 };
